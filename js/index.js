a164216033061edb3f1157248d86272e804caad0
hyebine <berryme015@gmail.com>
2024-03-06 17:34:10 +0900

    header 반응형 작업

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+  // 모바일 토글
+
+  document.querySelector(".btn").addEventListener("click", function () {
+    document.querySelector(".gnb").classList.toggle("act");
+  });
+
+
+
+  // 메인 스와이퍼
+
+
+})
