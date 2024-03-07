e10c30e195da74c2ccb80470e9c2b1f10a391e97
hyebine <berryme015@gmail.com>
2024-03-07 17:41:16 +0900

    gnb 반응형 , 메인 스와이퍼 생성

--- js/index.js
+++ js/index.js
@@ -1,14 +1,30 @@
-$(document).ready(function () {
 
-  // 모바일 토글
+// 모바일 토글
 
-  document.querySelector(".btn").addEventListener("click", function () {
-    document.querySelector(".gnb").classList.toggle("act");
-  });
-
-
-
-  // 메인 스와이퍼
+document.getElementById("btn").addEventListener("click", function () {
+  var gnb = document.querySelector(".gnb");
+  gnb.classList.toggle("act");
+  this.classList.toggle("change");
+});
 
 
-})
+
+
+// 메인 스와이퍼
+
+var mainswiper = new Swiper("#mainslide .swiper", {
+  loop: true,
+  effect: "fade",
+  navigation: {
+    nextEl: "#mainslide .swiper-button-next",
+    prevEl: "#mainslide .swiper-button-prev",
+  },
+  pagination: {
+    el: "#mainslide .swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+});
